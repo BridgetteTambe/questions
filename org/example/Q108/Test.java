a20c677380665c5f20abbe9c4b1b857ce84ab580
BridgetteTambe <bridgettetambe@gmail.com>
2023-10-23 20:17:30 +0200

    questions added

--- org/example/Q108/Test.java
+++ org/example/Q108/Test.java
@@ -6,49 +6,20 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Test {
+    public static void main(String[] args) throws SQLException {
 
 
 
-        public static void main(String[] args) {
-            try(Connection conn = ds.getConnectio());
-            PreparedStatement ps = conn.PrepareStatement("INSERT INTO EMP VALUES(?,?,?)")){
-
-                try {
-                ps.setObject(1, 101, JDBCType.INTEGER);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            try {
-                ps.setObject(2, "SMITH", JDBCType.VARCHAR);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            try {
-                ps.setObject(3, "HR", JDBCType.VARCHAR);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            try {
-                ps.executeUpdate();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            try {
-                ps.setInt(1,102);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            try {
-                ps.setString(2,"JONES");
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            try {
-                ps.executeUpdate();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-
+        try(Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/BANK_DB", "root", "pvi@2022");
+            PreparedStatement ps = conn.prepareStatement("INSERT INTO EMP VALUES(?,?,?)")){
+            ps.setObject(1, 101, JDBCType.INTEGER);
+            ps.setObject(2, "SMITH", JDBCType.VARCHAR);
+            ps.setObject(3, "HR", JDBCType.VARCHAR);
+            ps.executeUpdate();
+            ps.setInt(1, 102);
+            ps.setString(2, "JONES");
+            ps.executeUpdate();
         }
-    }
+        }
+
 }
